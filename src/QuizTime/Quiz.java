2ee7d9d108b052bbe5536ab5abdf5cbea1c87781
13744695 <HelenaRothwell@gmail.com>
2015-06-17 20:57:45 +0100

    More added to quiz file

--- src/QuizTime/Quiz.java
+++ src/QuizTime/Quiz.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
  * On choosing a topic the user is presented with a number of questions on that topic and...
  * an option to type go when they've had time to think about the answers to see the solutions
  * 
- * Decisions to be made on how to progress from one topic to next *****************************************************************************************************************
  * 
  */
 
@@ -28,14 +27,10 @@ public class Quiz {
 		
 	}
 	
-		public static void sc(){
-			Scanner sc = new Scanner(System.in);
-			System.out.println("When you are ready to see the answers type \"go\"");
-			String go = sc.nextLine();
-		}
+
 		public static void openQuiz() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose from the following options \n r for random category questions \n c for constructor type questions \n s for 'static' type questions \n f for 'final' related questions \n x for counting questions");
+		System.out.println("Choose from the following options \n r for random category questions \n c for constructor type questions \n s for 'static' type questions \n f for 'final' related questions \n x for counting questions \n or 'exit'");
 		String ans1 = sc.nextLine();
 		
 		switch (ans1){
@@ -44,17 +39,17 @@ public class Quiz {
 		case "s" : stat(); break;
 		case "f" : fin(); break;
 		case "x" : count(); break;
-		default: openQuiz();						// invalid value then run the openQuiz method again
+		case "exit" : System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!"); break;
+		default: openQuiz();						// invalid value: then run the openQuiz method again
 		}
 		}
-		
+	//__________________________________________________________________________________________________________________________________________	
 		public static void random(){
 		System.out.println("RANDOM CATEGORY QUESTIONS");
 		System.out.println();
-		System.out.println("How do you make the editor full screen in eclipse? ");
-		System.out.println();
+		System.out.println("What's the shortcut to make the editor full screen in eclipse? ");
+
 		System.out.println("What's the eclipse shortcut for the print statement? ");
-		System.out.println();
 		System.out.println("What would you use for an if statement when you want to output 'right' if 'ans' is equal to 'black'");
 		System.out.println();
 		System.out.println();
@@ -68,31 +63,67 @@ public class Quiz {
 						System.out.println("     "+"Ctrl M makes the editor fullscreen in eclipse, Ctrl M again to undo.");
 						System.out.println( "     "+"sysout and ctrl and space gives - System.out.println();");
 						System.out.println("     "+"You can't use == for strings. You must use ans.equals(\"black\")");
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						
+						Scanner sc1 = new Scanner(System.in);
+						System.out.println("WHEN READY TO GO BACK TO CHOICES TYPE \"go\" OR \"exit\" to exit");
+						String go1 = sc.nextLine();	
+						 if (go1.equalsIgnoreCase("go")){
+							 openQuiz();
+						 }
+						 else if(go1.equalsIgnoreCase("exit")){
+							System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!");
+						 }
 		}
 		
 		}
+		//__________________________________________________________________________________________________________________________________________	
 		
 		public static void construct(){
 		System.out.println("CONSTRUCTOR QUESTIONS");
 		System.out.println();
-		System.out.println("What's special about naming a constructor? : ");
-		System.out.println("What are constructors used for? : ");
-		System.out.println("What happens to a constructor everytime an object is created? :");
-		System.out.println("Can a constructor take parameters like a method does? :");
-		System.out.println("Can there be different constructors named the same? :");
-		System.out.println();
-		System.out.println();
-						// Constructors must have the same name as the class.
-						// Constructors are often used to initialise instance variables
-						// Constructor run every time an object is created.
-						// Constructors can take parameters in the same way a method can
-						// As with methods, you can have several different constructors with the very same name
-						// for as long as each one takes in different parameters 
-						// java will figure out which one to call based on the combination of parameters fed into it
-
+		System.out.println("What's special about naming a constructor?  ");
+		System.out.println("What are constructors used for? ");
+		System.out.println("What happens to a constructor everytime an object is created? ");
+		System.out.println("Can a constructor take parameters like a method does? ");
+		System.out.println("Can there be different constructors named the same? ");
 		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("WHEN READY TO VIEW ANSWERS TYPE \"go\"");
+		String go = sc.nextLine();
+		
+		if (go.equals("go")){		
+						System.out.println("     "+"Constructors must have the same name as the class.");
+						System.out.println("     "+"Constructors are often used to initialise instance variables");
+						System.out.println("     "+"Constructor run every time an object is created.");
+						System.out.println("     "+"Constructors can take parameters in the same way a method can");
+						System.out.println("     "+"As with methods, you can have several different constructors with the very same name");
+						System.out.println("     "+"for as long as each one takes in different parameters ");
+						System.out.println("     "+"java will figure out which one to call based on the combination of parameters fed into it");
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						
+						Scanner sc1 = new Scanner(System.in);
+						System.out.println("WHEN READY TO GO BACK TO CHOICES TYPE \"go\" OR \"exit\" to exit");
+						String go1 = sc.nextLine();	
+						 if (go1.equalsIgnoreCase("go")){
+							 openQuiz();
+						 }
+						 else if(go1.equalsIgnoreCase("exit")){
+							System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!");
+						 }
 		}
 		
+		}
+
+		//__________________________________________________________________________________________________________________________________________	
 		public static void stat(){
 		System.out.println("STATIC KEYWORD QUESTIONS");
 		System.out.println();
@@ -108,24 +139,51 @@ public class Quiz {
 		System.out.println("When do static instance variables exist? Before or after you create objects? :");
 		System.out.println("When do non-static class variables exist? Before or after you create objects? :");
 		System.out.println("What are non-static class variables associated with? :");
+		
 		System.out.println();
-						// non static variables are called instance variables. 
-						// static variables also known as class variables belongs to the class. There's only one copy, as there's only one class.
-						// So to access desc, you go through the class. Thing.desc = "I am a static class variable of type thing";
-						// each object does not get it's own copy. 
-						// Static Methods:
-						// Similar to static variables -
-						// You call them through the class name - Thingzoid.showInfo();
-						// Whereas non-static methods are called through the object.
-						// Static methods can access static variables
-						// they can't access instance variables (non-static)
-						// Can't make a static reference to a non-static field name
-						// Static instance variables exist before you even create any objects 
-						// The Static method can't know about non-static variables as they are associated with particular objects
-						// However non-static (Instance) methods can access static data. 
-						// By the time you've got objects you've got a class
+		System.out.println();
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("WHEN READY TO VIEW ANSWERS TYPE \"go\"");
+		String go = sc.nextLine();
+		
+		if (go.equals("go")){		
+						System.out.println("     "+"non static variables are called instance variables. ");
+						System.out.println("     "+"static variables also known as class variables belongs to the class. There's only one copy, as there's only one class.");
+						System.out.println("     "+"So to access desc, you go through the class. Thing.desc = \"I am a static class variable of type thing\";");
+						System.out.println("     "+"each object does not get it's own copy. ");
+						System.out.println("     "+"Static Methods:");
+						System.out.println("     "+"Similar to static variables -");
+						System.out.println("     "+"You call them through the class name - Thingzoid.showInfo();");
+						System.out.println("     "+"Whereas non-static methods are called through the object.");
+						System.out.println("     "+"Static methods can access static variables");
+						System.out.println("     "+"they can't access instance variables (non-static)");
+						System.out.println("     "+"Can't make a static reference to a non-static field name");
+						System.out.println("     "+"Static instance variables exist before you even create any objects ");
+						System.out.println("     "+"The Static method can't know about non-static variables as they are associated with particular objects");
+						System.out.println("     "+"However non-static (Instance) methods can access static data. ");
+						System.out.println("     "+"By the time you've got objects you've got a class");
+						System.out.println("     "+"");
+						
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						
+						Scanner sc1 = new Scanner(System.in);
+						System.out.println("WHEN READY TO GO BACK TO CHOICES TYPE \"go\" OR \"exit\" to exit");
+						String go1 = sc.nextLine();	
+						 if (go1.equalsIgnoreCase("go")){
+							 openQuiz();
+						 }
+						 else if(go1.equalsIgnoreCase("exit")){
+							System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!");
+						 }
 		}
 		
+		}
+
+		//__________________________________________________________________________________________________________________________________________	
 		public static void fin() {
 		
 		System.out.println("Some 'final' questions");
@@ -133,36 +191,86 @@ public class Quiz {
 		System.out.println("Why or how would you use the 'final' field? :");
 		System.out.println("What are the naming conventions in relation to using a final field? :");
 		System.out.println("What are the rules in relation to assigning a value when using a final field? :");
+
 		System.out.println();
-						// "     public final static int UNLUCKY_NUMBER= 13;		"			
-						// Constant variables such as PI are represented in java by capital letters. 
-						// and so our own constant variable is also represented here by capitalised name. 
-						// The final here is to prevent reassigning another constant to the 'variable' UNLuCKY_NUMBER
-						// When you have a final field you must always set that variable equal to something when you declare it.
-						// "     System.out.println(Math.PI);	 "						
-						// Math.PI is an example of a static class available from the java libraries, note it is also capitalised
+		System.out.println();
+		System.out.println();
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("WHEN READY TO VIEW ANSWERS TYPE \"go\"");
+		String go = sc.nextLine();
+		
+		if (go.equals("go")){		
+						System.out.println("     "+"\"     public final static int UNLUCKY_NUMBER= 13;		\"");
+						System.out.println("     "+"Constant variables such as PI are represented in java by capital letters.");
+						System.out.println("     "+"and so our own constant variable is also represented here by capitalised name. ");
+						System.out.println("     "+"The final here is to prevent reassigning another constant to the 'variable' UNLuCKY_NUMBER");
+						System.out.println("     "+"When you have a final field you must always set that variable equal to something when you declare it.");
+						System.out.println("     "+"\"     System.out.println(Math.PI);	 \"");
+						System.out.println("     "+"Math.PI is an example of a static class available from the java libraries, note it is also capitalised");
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						
+						Scanner sc1 = new Scanner(System.in);
+						System.out.println("WHEN READY TO GO BACK TO CHOICES TYPE \"go\" OR \"exit\" to exit");
+						String go1 = sc.nextLine();	
+						 if (go1.equalsIgnoreCase("go")){
+							 openQuiz();
+						 }
+						 else if(go1.equalsIgnoreCase("exit")){
+							System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!");
+						 }
 		}
 		
+		
+		}
+
+		//__________________________________________________________________________________________________________________________________________	
 		public static void count() {
 		System.out.println("Some counting questions");
 		System.out.println();
 		System.out.println("What's needed to count number of objects created in the MAIN class? :");
 		System.out.println("What's needed to count number of objects created in the object class? :");
-		System.out.println();
 
-						// The following would go in the main class
-						// "		System.out.println("Before I create a Thingzoid object count is: " +Thingzoid.count);			"
-						// "		Thingzoid t1 = new Thingzoid();																	"
-						// "		Thingzoid t2 = new Thingzoid();																	"
-						// "		System.out.println("After I create some Thingzoid objects, count is: " +Thingzoid.count);		"
-						// The following would go in the object class
-						// "		public static int count = 0;																	"
-						// because this is a static variable, we know it belongs to the class and not to each object.
-						// "		public Thingzoid(){																				"
-						// "			count ++;																					"
-						// "		}																								"
-						// this is a CONSTRUCTOR created specifically to count the number of times the class is accessed.
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("WHEN READY TO VIEW ANSWERS TYPE \"go\"");
+		String go = sc.nextLine();
+		
+		if (go.equals("go")){		
+						System.out.println("     "+"The following would go in the main class");
+						System.out.println("     "+"\"		System.out.println(\"Before I create a Thingzoid object count is: \" +Thingzoid.count);			\"");
+						System.out.println("     "+"\"		Thingzoid t1 = new Thingzoid();																	\"");
+						System.out.println("     "+"\"		Thingzoid t2 = new Thingzoid();																	\"");
+						System.out.println("     "+"\"		System.out.println(\"After I create some Thingzoid objects, count is: \" +Thingzoid.count);		\"");
+						System.out.println("     "+"");
+						System.out.println("     "+"The following would go in the object class");
+						System.out.println("     "+"\"		public static int count = 0;																	\"");
+						System.out.println("     "+"because this is a static variable, we know it belongs to the class and not to each object.");
+						System.out.println("     "+"\"		public Thingzoid(){																				\"");
+						System.out.println("     "+"\"			count ++;																					\"");
+						System.out.println("     "+"\"		}																								\"");
+						System.out.println("     "+"this is a CONSTRUCTOR created specifically to count the number of times the class is accessed.");
+						System.out.println("     "+"");
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						
+						Scanner sc1 = new Scanner(System.in);
+						System.out.println("WHEN READY TO GO BACK TO CHOICES TYPE \"go\" OR \"exit\" to exit");
+						String go1 = sc.nextLine();	
+						 if (go1.equalsIgnoreCase("go")){
+							 openQuiz();
+						 }
+						 else if(go1.equalsIgnoreCase("exit")){
+							System.out.println("Thank you for taking our quiz, we hope you learned something while you were here. \n Happy Java!!");
+						 }
+		}
+		
 		
 		}
 	
